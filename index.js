d407747ffcf4bce3604f25ad34821c58bbbca6db
unknown <YY340@ccs.local>
2025-02-06 05:28:41 -0800

    Uploading the folder my-express-app

---
+++ index.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const app = express();
+const port = 7000;
+
+// Serve static files from the "public" folder
+app.use(express.static('public'));
+
+// Define a route for the home page
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
+// Add a new route to handle a GET request:
+app.get('/about', (req, res) => {
+    res.send('Aboust Us');
+});
+
+app.use(express.json()); // Middleware to parse JSON bodies
+
+app.post('/submit', (req, res) => {
+    const data = req.body;
+    res.send(`Received: ${JSON.stringify(data)}`);
+});
+
+// Error Handling Middleware
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send('Something broke!');
+});
+
+// Start the server
+app.listen(port, () => {
+    console.log(`Server running at http://localhost:${port}`);
+});
+
+// Add a new route to displa a list of items
+const items = ['Apple','Banana','Orange'];
+
+// Add a new route to Handle Item
+app.post('items', (req, res) => {
+    const newItem = req.body.item;
+    items.push(newItem);
+    res.json(items);
+});
+
+app.get('/items', (req, res) => {
+    res.json(items);
+});
